e9892d14aa57eed264508291d17742193d4bb946
calcuis <113646141+calcuis@users.noreply.github.com>
2022-11-16 10:07:24 +0800

    Add files via upload

---
+++ comparegroup.js
@@ -0,0 +1,35 @@
+class GroupErr extends react.exports.Component {
+  render() {
+    var group1 = {
+      x: ["Jul", "Aug", "Sep", "Oct", "Nov"],
+      y: [250, 200, 300, 450, 500],
+      name: "Upper Class",
+      error_y: {
+        type: "data",
+        array: [15, 10, 15, 10, 20],
+        visible: true
+      },
+      type: "bar"
+    };
+    var group2 = {
+      x: ["Jul", "Aug", "Sep", "Oct", "Nov"],
+      y: [150, 250, 250, 350, 400],
+      name: "Lower Class",
+      error_y: {
+        type: "data",
+        array: [20, 15, 20, 15, 10],
+        visible: true
+      },
+      type: "bar"
+    };
+    var data = [group1, group2];
+    return /* @__PURE__ */ jsx(Plot, {
+      data,
+      layout: {
+        width: 800,
+        height: 600,
+        title: "Monthly income (**)"
+      }
+    });
+  }
+}
